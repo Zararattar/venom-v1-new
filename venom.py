5052e1435133efb1a89dfb1ef36539ef886c1849
Zararattar <zarargaming186@gmail.com>
2024-09-08 01:53:32 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7505319950:AAFa-bTZiOLkRuTeZzhIwMXsw-7yQd8z1eM'
+ADMIN_ID = '6420244768'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
